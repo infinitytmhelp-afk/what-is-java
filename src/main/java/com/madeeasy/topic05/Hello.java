d17cb1823348df274bd8423ed52fb8837b18814f
Pabitra Bera <madeeasycodinglife@gmail.com>
2026-07-20 10:35:57 +0530

    feat: topic 5

---
+++ src/main/java/com/madeeasy/topic05/Hello.java
@@ -0,0 +1,11 @@
+package com.madeeasy.topic05;
+
+public class Hello {
+    public static void main(String[] args) {
+       for (int i = 1; i <= 100; i++) {
+           System.out.println("Hello, Java!");
+           System.out.println("Hello, Viewers!");
+           System.out.println("Hello, It-World!");
+       }
+    }
+}
